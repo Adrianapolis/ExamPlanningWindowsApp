106e29bc242b97bfe5b94e2b94a406d668ab8280
ugurkiymetli <ugurkiymetli@hotmail.com>
2017-12-13 14:02:53 +0300

    Kullanıcı Kontrolleri
    Kayıt ekleme sayfalarına 'boş alan' kontrolü eklendi.
    Anasayfadaki Butonlara iligili verileri yükleme eklendi.
    Database class'ında kod okunurluğu arttırıldı.

--- WindowsApplication3/Form_Asistan.vb
+++ WindowsApplication3/Form_Asistan.vb
@@ -1,8 +1,17 @@
 ﻿Public Class Form_Asistan
     Private Sub Button_AsistanEkle_Click(sender As Object, e As EventArgs) Handles btnAsistanEkle.Click
         Dim a As New Asistan()
-        a.AsistanAdi = tbAsistanAdi.Text
-        a.Mail = tbAsistanMail.Text
-        database.AsistanEkle(a)
+        If tbAsistanAdi.Text = "" Or tbAsistanMail.Text = "" Then
+            MessageBox.Show("Alanlar boş geçilemez.")
+            tbAsistanAdi.Clear()
+            tbAsistanMail.Clear()
+        Else
+            a.AsistanAdi = tbAsistanAdi.Text
+            a.Mail = tbAsistanMail.Text
+            database.AsistanEkle(a)
+            MessageBox.Show("Başarıyla Eklendi.")
+            tbAsistanAdi.Clear()
+            tbAsistanMail.Clear()
+        End If
     End Sub
 End Class
--- WindowsApplication3/Form_Derslik.vb
+++ WindowsApplication3/Form_Derslik.vb
@@ -1,10 +1,18 @@
 ﻿Public Class Form_Derslik
     Private Sub Button_DerslikOlustur_Click(sender As Object, e As EventArgs) Handles btnDerslikOlustur.Click
-        Dim eklencekderslik As New Derslik
-        eklencekderslik.DerslikAdi = tbDerslikAdi.Text
-        eklencekderslik.Kapasite = Convert.ToInt32(tbDerslikKapasite.Text)
-        database.DerslikEkle(eklencekderslik)
-        tbDerslikAdi.Clear()
-        tbDerslikKapasite.Clear()
+        Dim derslik As New Derslik
+        If tbDerslikAdi.Text = "" Or tbDerslikKapasite.Text = "" Then
+            MessageBox.Show("Alanlar boş geçilemez.")
+            tbDerslikAdi.Clear()
+            tbDerslikKapasite.Clear()
+        Else
+            derslik.DerslikAdi = tbDerslikAdi.Text
+            derslik.Kapasite = Convert.ToInt32(tbDerslikKapasite.Text)
+            database.DerslikEkle(derslik)
+            MessageBox.Show("Başarıyla Eklendi.")
+            tbDerslikAdi.Clear()
+            tbDerslikKapasite.Clear()
+        End If
     End Sub
 End Class
+
